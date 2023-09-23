4cb77928f7e5eeff1c0abac5ee8fea86080e0856
RizvanGit <imeandmyep2@gmail.com>
2023-09-23 20:41:53 +0300

    fix: config change gitsigns

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -1,14 +1,14 @@
 require('gitsigns').setup {
   signs = {
-    add          = { text = '│' },
+    add          = { text = '' },
     change       = { text = '│' },
-    delete       = { text = '_' },
+    delete       = { text = 'x' },
     topdelete    = { text = '‾' },
     changedelete = { text = '~' },
     untracked    = { text = '┆' },
   },
   signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
-  numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
+  numhl      = true, -- Toggle with `:Gitsigns toggle_numhl`
   linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
   word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
   watch_gitdir = {
